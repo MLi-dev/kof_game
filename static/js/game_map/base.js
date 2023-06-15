b80df103150940ad8cd1270818a9eccb646d2cbb
Matthew Li <matthewli2003@gmail.com>
2023-06-14 21:56:44 -0700

    Added gifs, player move and direction.

--- static/js/game_map/base.js
+++ static/js/game_map/base.js
@@ -1,21 +1,22 @@
 import { AcGameObject } from "../ac_game_object/base.js";
+import { Controller } from "../controller/base.js";
 
 class GameMap extends AcGameObject {
 	constructor(root) {
 		super();
 		this.root = root;
-		this.$canvas = $('<canvas width="1280" height="720" tbindex=0></canvas>');
+		this.$canvas = $('<canvas width="1280" height="720" tabindex=0></canvas>');
 		this.root.$kof.append(this.$canvas);
 		this.ctx = this.$canvas[0].getContext("2d");
 		this.$canvas.focus();
+		this.controller = new Controller(this.$canvas);
 	}
 	start() {}
 	update() {
 		this.render();
 	}
 	render() {
-		this.ctx.fillStyle = "black";
-		this.ctx.fillRect(0, 0, this.$canvas.width(), this.$canvas.height());
+		this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 	}
 }
 
--- static/js/player/base.js
+++ static/js/player/base.js
@@ -10,14 +10,116 @@ class Player extends AcGameObject {
 		this.height = info.height;
 		this.color = info.color;
 		this.ctx = this.root.game_map.ctx;
+		this.direction = 1;
+		this.vx = 0;
+		this.vy = 0;
+		this.speedx = 400;
+		this.speedy = -1000;
+		this.gravity = 50;
+		this.pressed_keys = this.root.game_map.controller.pressed_keys;
+		this.status = 3;
+		this.animations = new Map();
+		this.frame_current_cnt = 0;
 	}
 	start() {}
+	update_move() {
+		this.vy += this.gravity;
+		this.x += (this.vx * this.timedelta) / 1000;
+		this.y += (this.vy * this.timedelta) / 1000;
+		if (this.y > 450) {
+			this.y = 450;
+			this.vy = 0;
+			if (this.status === 3) this.status = 0;
+		}
+	}
+	update_control() {
+		let w, a, d, space;
+		if (this.id == 0) {
+			a = this.pressed_keys.has("a");
+			w = this.pressed_keys.has("w");
+			d = this.pressed_keys.has("d");
+			space = this.pressed_keys.has(" ");
+		} else {
+			a = this.pressed_keys.has("ArrowLeft");
+			w = this.pressed_keys.has("ArrowUp");
+			d = this.pressed_keys.has("ArrowRight");
+			space = this.pressed_keys.has("Enter");
+		}
+		if (this.status === 0 || this.status === 1) {
+			if (w) {
+				if (d) {
+					this.vx += this.speedx;
+				} else if (a) {
+					this.vx -= this.speedx;
+				} else {
+					this.vx = 0;
+				}
+				this.vy += this.speedy;
+				this.status = 3;
+			} else if (a) {
+				this.vx = -this.speedx;
+				this.status = 1;
+			} else if (d) {
+				this.vx = this.speedx;
+				this.status = 1;
+			} else if (space) {
+				this.frame_current_cnt = 0;
+				this.status = 4;
+				this.vx = 0;
+			} else {
+				this.status = 0;
+				this.vx = 0;
+			}
+		}
+	}
+	update_direction() {
+		let players = this.root.players;
+		if (players[0] && players[1]) {
+			let me = this,
+				you = players[1 - this.id];
+			if (me.x < you.x) me.direction = 1;
+			else me.direction = -1;
+		}
+	}
 	update() {
+		this.update_control();
+		this.update_move();
+		this.update_direction();
 		this.render();
 	}
+
 	render() {
-		this.ctx.fillStyle = this.color;
-		this.ctx.fillRect(this.x, this.y, this.width, this.height);
+		let status = this.status;
+		let obj = this.animations.get(status);
+		if (obj && obj.loaded) {
+			if (this.direction === 1) {
+				let k =
+					parseInt(this.frame_current_cnt / obj.frame_rate) % obj.frame_cnt;
+				let image = obj.gif.frames[k].image;
+				this.ctx.drawImage(
+					image,
+					this.x,
+					this.y + obj.offset_y,
+					image.width * obj.scale,
+					image.height * obj.scale
+				);
+			} else {
+				this.ctx.save();
+				this.ctx.scale(-1, 1);
+				this.ctx.translate(-this.root.game_map.$canvas.width(), 0);
+				let k =
+					parseInt(this.frame_current_cnt / obj.frame_rate) % obj.frame_cnt;
+				let image = obj.gif.frames[k].image;
+				this.ctx.drawImage(
+					image,
+					this.root.game_map.$canvas.width() - this.x - this.width,
+					this.y + obj.offset_y,
+					image.width * obj.scale,
+					image.height * obj.scale
+				);
+				this.ctx.restore();
+			}
+		}
 	}
 }
 export { Player };
